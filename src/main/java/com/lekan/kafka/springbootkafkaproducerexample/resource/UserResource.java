afc390a17e7d48e4fa159f249604c0026d035af9
Lekan701 <olalekan.busari@outlook.com>
2024-01-25 12:26:44 +0000

    Created Kafka producer which sends messages to a topic on my local, run docker local to access control center to check the messages :)

---
+++ src/main/java/com/lekan/kafka/springbootkafkaproducerexample/resource/UserResource.java
@@ -0,0 +1,27 @@
+package com.lekan.kafka.springbootkafkaproducerexample.resource;
+
+import com.lekan.kafka.springbootkafkaproducerexample.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+
+    @Autowired
+    KafkaTemplate<String, User> kafkaTemplate;
+    private static final String TOPIC = "NewTopic1";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name){
+
+        kafkaTemplate.send(TOPIC, new User(name,"Tech", 1000L));
+
+        return "Published Successfully";
+    }
+
+}
